5ebe8a42c1a98143e4efbab2effdb61a5b7cc8bd
Ben John <github@benjohn.de>
2018-02-01 09:04:35 +0100

    refactor: restructured project for upcoming example

---
+++ DIKitExample/Sources/NetworkLayer/Network.swift
@@ -0,0 +1,16 @@
+//
+//  Network.swift
+//  DIKitExample
+//
+//  Created by Ben John on 01.02.18.
+//  Copyright © 2018 Ben John. All rights reserved.
+//
+
+import Foundation
+
+class Network: NetworkProtocol {
+    init() {
+        print("Network init")
+    }
+}
+
---
+++ DIKitExample/Sources/NetworkLayer/NetworkProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkProtocol.swift
+//  DIKitExample
+//
+//  Created by Ben John on 01.02.18.
+//  Copyright © 2018 Ben John. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkProtocol {
+    
+}
