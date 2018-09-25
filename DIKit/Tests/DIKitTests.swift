c76b95635405b2d45da8e0c9ad39a78d2bf6f44e
Ben John <github@benjohn.de>
2018-09-25 16:29:03 +0200

    add: created more tests

--- DIKit/Tests/DIKitTests.swift
+++ DIKit/Tests/DIKitTests.swift
@@ -29,6 +29,45 @@ class DIKitTests: XCTestCase {
             c.register { ComponentB() }
         }
 
+        guard let componentA = dependencyContainer.componentStack.index(forKey: "ComponentA") else {
+            return XCTFail()
+        }
+        let componentProtocolA = dependencyContainer.componentStack[componentA].value
+        XCTAssertEqual(componentProtocolA.lifetime, .singleton)
+        let instanceA = componentProtocolA.componentFactory()
+        XCTAssertTrue(instanceA is ComponentA)
+        XCTAssertFalse(instanceA is ComponentB)
+
+        guard let componentB = dependencyContainer.componentStack.index(forKey: "ComponentB") else {
+            return XCTFail()
+        }
+        let componentProtocolB = dependencyContainer.componentStack[componentB].value
+        XCTAssertEqual(componentProtocolB.lifetime, .singleton)
+        let instanceB = componentProtocolB.componentFactory()
+        XCTAssertTrue(instanceB is ComponentB)
+        XCTAssertFalse(instanceB is ComponentA)
+    }
+
+    func testDependencyContainerResolve() {
+        struct ComponentA {}
+
+        let dependencyContainer = DependencyContainer { (c: DependencyContainer) in
+            c.register { ComponentA() }
+        }
+
+        let componentA: ComponentA = dependencyContainer.resolve()
+        XCTAssertNotNil(componentA)
+    }
+
+    func testLifetimeOfComponents() {
+        struct ComponentA {}
+        struct ComponentB {}
+
+        let dependencyContainer = DependencyContainer { (c: DependencyContainer) in
+            c.register { ComponentA() }
+            c.register { ComponentB() }
+        }
+
         let componentA = dependencyContainer.componentStack.index(forKey: "ComponentA")
         XCTAssertNotNil(componentA)
 
