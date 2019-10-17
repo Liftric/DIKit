848240c5a7312c9d27b80860458c112922d8cb50
Ben John <john@liftric.com>
2019-10-17 09:31:21 +0200

    refactor: rework usage of DefinesContainer (do not limit to AppDelegate)

--- DIKit/Sources/Container/DefinesContainer.swift
+++ DIKit/Sources/Container/DefinesContainer.swift
@@ -7,7 +7,7 @@
 //
 // Copyright © 2018 Ben John. All rights reserved.
 
-/// Exposes the `DependencyContainer` through `AppDelegate`.
+/// Exposes the `DependencyContainer` through for instance `AppDelegate`.
 public protocol DefinesContainer {
     var container: DependencyContainer { get }
 }
